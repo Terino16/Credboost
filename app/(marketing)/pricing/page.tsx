1a0fd497403514e43d392904bdd3c67cc77af5e5
Aatreya <padmajaatreya02@gmail.com>
2025-02-16 14:54:14 +0530

    feat: pricing cards added

--- app/(marketing)/pricing/page.tsx
+++ app/(marketing)/pricing/page.tsx
@@ -1,7 +1,17 @@
+import PricingCard from "@/components/pricing/PricingCard";
+
 export default function Pricing() {
-    return (
-        <div>
-            <h1>Pricing</h1>
-        </div>
-    )
+  return (
+    <div className="py-24 sm:py-30">
+      <h2 className="text-3xl md:text-4xl font-bold text-center">
+        Get
+        <span className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text animate-gradient">
+        &nbsp;
+          Unlimited &nbsp;
+        </span>
+        Access For Your Account
+      </h2>
+      <PricingCard />
+    </div>
+  );
 }
---
+++ components/pricing/PricingCard.tsx
@@ -0,0 +1,76 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Check } from "lucide-react";
+import { pricingList, PricingProps, PopularPlanType } from "@/app/(marketing)/pricing/pricingData";
+
+
+export default function PricingCard () {
+  return (
+    <section
+      id="pricing"
+      className="container py-6 sm:py-10"
+    >
+      
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {pricingList.map((pricing: PricingProps) => (
+          <Card
+            key={pricing.title}
+            className={
+              pricing.popular === PopularPlanType.YES
+                ? "drop-shadow-xl shadow-black/10 dark:shadow-white/10"
+                : ""
+            }
+          >
+            <CardHeader>
+              <CardTitle className="flex item-center justify-between">
+                {pricing.title}
+                {pricing.popular === PopularPlanType.YES ? (
+                  <Badge
+                    variant="secondary"
+                    className="text-sm text-primary bg-indigo-500"
+                  >
+                    Most popular
+                  </Badge>
+                ) : null}
+              </CardTitle>
+              <div>
+                <span className="text-3xl font-bold">${pricing.price}</span>
+                <span className="text-muted-foreground"> /month</span>
+              </div>
+
+              <CardDescription>{pricing.description}</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <Button className="w-full">{pricing.buttonText}</Button>
+            </CardContent>
+
+            <hr className="w-4/5 m-auto mb-4" />
+
+            <CardFooter className="flex">
+              <div className="space-y-4">
+                {pricing.benefitList.map((benefit: string) => (
+                  <span
+                    key={benefit}
+                    className="flex"
+                  >
+                    <Check className="text-[#22C461] " />{" "}
+                    <h3 className="ml-2">{benefit}</h3>
+                  </span>
+                ))}
+              </div>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </section>
+  );
+};
