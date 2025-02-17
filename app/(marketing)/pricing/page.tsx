d83d5a0c50ad3e8cd63b8a65b6cc03e6d8c5f3ca
Aatreya <padmajaatreya02@gmail.com>
2025-02-17 13:56:00 +0530

    chore: pricing page responsive + navbar home redirect

--- app/(marketing)/pricing/page.tsx
+++ app/(marketing)/pricing/page.tsx
@@ -2,7 +2,7 @@ import PricingCard from "@/components/pricing/PricingCard";
 
 export default function Pricing() {
   return (
-    <div className="py-24 sm:py-30">
+    <div className="flex flex-col items-center px-8 md:px-2 py-4 sm:py-8">
       <h2 className="text-3xl md:text-4xl font-bold text-center">
         Get
         <span className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text animate-gradient">
--- components/marketing/Navbar.tsx
+++ components/marketing/Navbar.tsx
@@ -1,56 +1,70 @@
-import Image from "next/image"
-import { Routes } from "@/constants/Route"
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
-import { ModeToggle } from "@/components/ui/theme-toggle"
-import MobileNavbar from "./MobileNavbar"
-import { LinkComponent } from "../ui/link"
-import { auth } from "@/lib/auth"
-import UserButton from "@/components/general/UserButton"
-
+import Image from "next/image";
+import { Routes } from "@/constants/Route";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ModeToggle } from "@/components/ui/theme-toggle";
+import MobileNavbar from "./MobileNavbar";
+import { LinkComponent } from "../ui/link";
+import { auth } from "@/lib/auth";
+import UserButton from "@/components/general/UserButton";
 
 export default async function Navbar() {
-    const session = await auth();
- 
-    
-    return (
-        <div className="  mx-auto flex max-w-6xl  justify-between items-center py-8 ">
-            {/* Desktop Navbar */}
-            <div className="hidden lg:flex justify-between items-center w-full ">
-                <div className="flex items-center gap-2">
-                    <Image src="/logo.png" alt="CredBoost" width={35} height={25} />
-                    <h1 className="text-xl font-semibold tracking-tight">CredBoost</h1>
-                </div>
-                <div className="flex items-center gap-16  text-gray-500">
-                    <LinkComponent href={Routes.dashboard} className=" text-[18px] font-light tracking-tight  ">Dashboard</LinkComponent>
-                    <LinkComponent href={Routes.pricing} className=" text-[18px] font-light tracking-tight  ">Pricing</LinkComponent>
-                    <LinkComponent href={Routes.features} className="text-[18px] font-light tracking-tight ">Features</LinkComponent>
-                    <LinkComponent href={Routes.blog} className=" text-[18px] font-light tracking-tight">Blog</LinkComponent>
-                </div>
-                <div className="flex items-center gap-4">
-                    <ModeToggle />
-                    {session?.user ? (
-                        <UserButton />
-                    ) : (
-                        <>
-                        <Button variant="default">
-                        <Link href={Routes.login}>
-                            Login
-                        </Link>
-                    </Button>
-                    <Button variant="outline">
-                        <Link href={Routes.signup}>
-                            Singup
-                        </Link>
-                    </Button>
-                    </>
-                    )}
-                </div>
-            </div>
-            {/* Mobile Navbar */}
-            <div className="lg:hidden w-full">
-                <MobileNavbar />
-            </div>
+  const session = await auth();
+
+  return (
+    <div className="  mx-auto flex max-w-6xl  justify-between items-center py-8 ">
+      {/* Desktop Navbar */}
+      <div className="hidden lg:flex justify-between items-center w-full ">
+        <Link href={Routes.home} className="flex items-center gap-2">
+          <Image src="/logo.png" alt="CredBoost" width={35} height={25} />
+          <h1 className="text-xl font-semibold tracking-tight">CredBoost</h1>
+        </Link>
+        <div className="flex items-center gap-16  text-gray-500">
+          <LinkComponent
+            href={Routes.dashboard}
+            className=" text-[18px] font-light tracking-tight  "
+          >
+            Dashboard
+          </LinkComponent>
+          <LinkComponent
+            href={Routes.pricing}
+            className=" text-[18px] font-light tracking-tight  "
+          >
+            Pricing
+          </LinkComponent>
+          <LinkComponent
+            href={Routes.features}
+            className="text-[18px] font-light tracking-tight "
+          >
+            Features
+          </LinkComponent>
+          <LinkComponent
+            href={Routes.blog}
+            className=" text-[18px] font-light tracking-tight"
+          >
+            Blog
+          </LinkComponent>
         </div>
-    )
+        <div className="flex items-center gap-4">
+          <ModeToggle />
+          {session?.user ? (
+            <UserButton />
+          ) : (
+            <>
+              <Button variant="default">
+                <Link href={Routes.login}>Login</Link>
+              </Button>
+              <Button variant="outline">
+                <Link href={Routes.signup}>Singup</Link>
+              </Button>
+            </>
+          )}
+        </div>
+      </div>
+      {/* Mobile Navbar */}
+      <div className="lg:hidden w-full">
+        <MobileNavbar />
+      </div>
+    </div>
+  );
 }
